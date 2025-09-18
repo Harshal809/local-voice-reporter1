0a8a574d44675cf17916bc87c70ade3189ba3318
AayushSahani01 <anshusahani6104@gmail.com>
2025-09-19 01:50:42 +0530

    TopArrow Icons Button Fixed!

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import Dashboard from "./pages/Dashboard";
 import IssueDetails from "./pages/IssueDetails";
 import Footer from "./components/ui/Footer";
 import NotFound from "./pages/NotFound";
+import OnTopBar from "./components/ui/TopBar";
 
 const queryClient = new QueryClient();
 
@@ -33,6 +34,7 @@ const App = () => (
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
+          <OnTopBar />
           <Footer />
         </div>
       </BrowserRouter>
---
+++ src/components/ui/TopBar.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+import { FaArrowUpLong } from "react-icons/fa6";
+const OnTopBar: React.FC = () => {
+  const [isvisible, setIsVisible] = React.useState(false);
+
+  const onTopbtn = () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
+  // Define the scroll event handler function *****
+  const handleScroll = () => {
+    
+
+    document.querySelector(".on-top-bar") as HTMLElement;
+    if (window.scrollY > 120) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  // Render the component AS useEffect
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return (
+    <button>
+        <div className="">
+        {isvisible && (
+        <div className="fixed bottom-6 right-5 text-xl font-bold z-50 bg-gray-800 hover:bg-black hover:bg-black/90 w-12 h-12 flex items-center justify-center  text-white p-3 rounded-full cursor-pointer transition-opacity duration-300 ease-in-out hover:scale-[1.1] focus:border-spacing-2 hover:border-spacing-4" onClick={onTopbtn}>
+          <FaArrowUpLong />
+        </div>
+      )}
+      </div>
+    </button>
+  );
+};
+
+export default OnTopBar;
